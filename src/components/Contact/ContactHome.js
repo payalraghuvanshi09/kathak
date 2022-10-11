217c5b33b95b5b93de6688a89f9f5375286b8b41
Payal Raghuvanshi <105650972+payalraghuvanshi09@users.noreply.github.com>
2022-10-11 16:26:54 +0530

    final commit

--- src/components/Contact/ContactHome.js
+++ src/components/Contact/ContactHome.js
@@ -96,7 +96,7 @@ function ContactHome ()  {
                         <h4>Open Hours:</h4>
                         <p>
                         Monday-Saturday:<br/>
-                        11:00 AM - 2300 PM
+                        11:00 AM - 6:00 PM
                         </p>
                     </div>
 
